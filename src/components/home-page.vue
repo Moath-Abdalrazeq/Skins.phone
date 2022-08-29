1d7d838f5d64510a7ce4bd37957abda32e0e5b92
warddaraghmeh <109684022+warddaraghmeh@users.noreply.github.com>
2022-08-29 14:08:08 +0300

    change viewAll and header Img

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -19,7 +19,7 @@ import scrollHeaderMenu from './scroll-header-menu.vue'
 <!-- header menu ----------------------------------------------------->
    
     <!--image header -------------------------------> 
- <div class=" h-96 lg:bg-cover  w-full bg-fixed bg-no-repeat  " :style="{'background-image': `url(${headerImg})`  }" >
+ <div class=" bg-contain h-96 lg:bg-cover  w-full bg-fixed bg-no-repeat  " :style="{'background-image': `url(${headerImg})`  }" >
  <scroll-header-menu></scroll-header-menu> 
    
  </div>
@@ -28,7 +28,7 @@ import scrollHeaderMenu from './scroll-header-menu.vue'
 <div class=" text-gray-400 text-center lg:p-10   text-xl   mt-10      ">
 <div>
     <span> Introducing GALAXY S22 ULTRA</span>
-    <router-link to="/homePage/viewS22Ultra" class="hidden justify-end lg:flex ">View all</router-link>
+    <router-link to="/homePage/viewS22Ultra" class=" mt-5 mb-5 justify-end flex ">View all</router-link>
    </div>   
    <div class="overflow-x-auto    ">
   <imge-samsung-s-22-ultra></imge-samsung-s-22-ultra>
@@ -38,10 +38,13 @@ import scrollHeaderMenu from './scroll-header-menu.vue'
   <div class="  text-gray-400 lg:p-10 lg:text-xl text-center mt-10  ">
    <div>
     <span>iPhone 13 Pro MAX Skins & iPhone 13 PRO Skins</span>
-    <router-link to="/homePage/viewIphone13" class="lg:flex justify-end hidden">View all</router-link>
+    
     </div>
     <div class=" ">
-    <base-slider></base-slider>  
+    <base-slider></base-slider>
+    <div class="mt-8 bg-orange-600 w-24  m-auto h-10 p-1  ">
+        <router-link to="/homePage/viewIphone13" class="lg:flex justify-center">View all</router-link>
+</div>
   </div>
   </div> 
   <footer-pages></footer-pages>
